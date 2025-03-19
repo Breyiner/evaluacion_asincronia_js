8ccce1e4a454d83626f6f9c30e37d0a97b71bea5
Breyiner <breyneracosta7@gmail.com>
2025-03-18 21:33:04 -0500

    solucion ejercicio 1, solicitar las tareas pendientes

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ const solicitarParametro = indicador => { // Definimos una función llamada "sol
 
     return respuesta; // Retornamos la respuesta válida ingresada por el usuario.
 }
+
+const tareasPendientes = async () => { // Definimos una función asíncrona llamada "tareasPendientes".
+    try { // Iniciamos un bloque try para manejar posibles errores.
+        const usuarios = await mod.getUsuarios(url); // Obtenemos la lista de usuarios llamando a la función "getUsuarios" con la URL proporcionada.
+
+        return await Promise.all( // Utilizamos Promise.all para esperar a que todas las promesas se resuelvan.
+            usuarios.map(async user => { // Iteramos sobre cada usuario utilizando el método "map".
+                const pendings = await mod.getTareasByUserIdStatus(url, user.id, false); // Obtenemos las tareas pendientes del usuario llamando a "getTareasByUser IdStatus".
+
+                return { // Retornamos un nuevo objeto que combina los datos del usuario con sus tareas pendientes.
+                    ...user, // Usamos el operador de propagación para incluir todas las propiedades del usuario.
+                    tareasPendientes: pendings // Añadimos una nueva propiedad "tareasPendientes" que contiene las tareas pendientes del usuario.
+                }
+            })
+        );
+    } catch (error) { // Capturamos cualquier error que ocurra en el bloque try.
+        console.error(`Error al obtener las tareas pendientes -> ${error}`); // Mostramos un mensaje de error en la consola con detalles del error.
+    }
+}
